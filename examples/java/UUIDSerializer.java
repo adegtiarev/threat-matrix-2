e15c988a91ab16faddd75bacb64f34462605dd4d
Andrei Degtiarev <adegtiarev@gmail.com>
2023-05-30 19:05:47 -0300

    update

--- examples/java/UUIDSerializer.java
+++ examples/java/UUIDSerializer.java
@@ -1,3 +1,18 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/FasterXML/jackson-databind/releases/tag/jackson-databind-2.14.2
+*    Source File: UUIDSerializer.java
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 package com.fasterxml.jackson.databind.ser.std;
 
 import java.io.IOException;
